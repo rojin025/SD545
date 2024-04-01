63cae176b324bfb244bbf5052ce690c779613aca
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-04-01 13:19:14 -0500

    v.1.0.2 - 2 completed. Happy coding :)

---
+++ Assignments/day1/README.md
@@ -0,0 +1,20 @@
+# SD545
+
+Web Development 1
+
+# Assignment 1
+
+Instructions
+
+1. Create a function using function declaration named sum with one parameter of Array type, the
+   returned result is the sum of all elements which are greater than 20.
+
+2. Create a function using function expression named getNewArray with one parameter of String
+   Array, return a new array which contains all string, length is greater than and equal to 5, and
+   contains letter ‘a’.
+
+3. Implement an arrow function with feature below:
+
+concat('hi', [1,2,3], ['Hello','world']) -> return result: ['h', 'i', 1,2,3, 'Hello','world']
+
+Please submit your github link here. Thanks
--- Assignments/day1/index.ts
+++ Assignments/day1/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello World.");
+// console.log("Hello World.");
+
+/**
+ * 1
+ * Create a function using 
+ * function declaration named 
+ * sum 
+        with one parameter of Array type, 
+        the returned result is the sum of all elements 
+        which are greater than 20.
+ */
+function sum(numbers: number[]): number {
+  let totalSum = numbers.reduce((total, curr) => total + curr, 0);
+  return totalSum;
+}
+
+console.log(sum([1, 2, 3]));
+console.log(sum([]));
+
+/**
+ * 
+2. 
+Create a function using function expression named 
+getNewArray with one parameter of String Array, 
+return a new array which contains all string, 
+length is greater than and equal to 5, 
+and contains letter ‘a’.
+ */
+const getNewArray = function (words: string[]): string[] {
+  let newWords = words.filter((word) => {
+    if (word.length >= 5 && word.includes("a")) {
+      return true;
+    }
+  });
+  return newWords;
+};
+
+console.log(getNewArray(["array", "word", "java", "Anguler"]));
