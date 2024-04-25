354cd891987b2fa4cc79120525ac0cd1a2522fa0
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-04-25 09:29:31 -0500

    Final 1.0.8 - Morning Revise Completed. Happy Coding :)

--- Assignments/Final-Review/src/App.tsx
+++ Assignments/Final-Review/src/App.tsx
@@ -3,9 +3,10 @@ import LoginUseReducer from "./Q2-UseReduce/LoginUseReducer";
 import LoginUseState from "./Q2-UseReduce/LoginUseState";
 import UseReducerSimple from "./Q2-UseReduce/useReducer/1.useReducer";
 import SomeReducerApp from "./Q2-UseReduce/useReducer/2.repet";
+import UseReducerObj from "./Q2-UseReduce/useReducer/3.useReducerObj";
+/**3 */
 import Root from "./Q3-Routers/router.root";
 import RootUseParams from "./Q3-Routers/usePraams/UseParam";
-import UseReducerObj from "./Q2-UseReduce/useReducer/3.useReducerObj";
 import NavBar from "./Q3-Routers/navBar";
 /** Q1 */
 import MemoHooks from "./Q1-Performace/useMemo";
@@ -15,13 +16,15 @@ function App() {
     <>
       <div>
         {/* <Root /> */}
+
+        <RootUseParams />
         {/* <UseReducerObj /> */}
-        {/* <RootUseParams /> */}
-        <MemoHooks />
+        {/* <MemoHooks /> */}
         {/* <LoginUseState /> */}
         {/* <LoginUseReducer /> */}
         {/* <CounterUseState /> */}
         {/* <UseReducerSimple /> */}
+
         {/* <SomeReducerApp /> */}
       </div>
     </>
--- Assignments/Final-Review/src/Q3-Routers/usePraams/NavBar.Prod.tsx
+++ Assignments/Final-Review/src/Q3-Routers/usePraams/NavBar.Prod.tsx
@@ -3,15 +3,17 @@ import { Link, NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <Link to="products/items">Items</Link>
-      </li>
-      <li>
-        <NavLink to="products/items/details">details</NavLink>
-      </li>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <Link to="products/items">Items</Link>
+        </li>
+        <li>
+          <NavLink to="products/items/details">details</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
